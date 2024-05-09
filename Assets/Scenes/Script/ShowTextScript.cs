e88151f6b2b7b4f86c83243edb37ac5cfef4fbed
Venena <venena.estere@gmail.com>
2024-05-09 09:18:31 +0300

    Sript sākums

---
+++ Assets/Scenes/Script/ShowTextScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI
+
+public class ShowTextScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Debug.Log("UZstartēts");
+
+    }
+int kadrs=0;
+    // Update is called once per frame
+    void Update()
+    {
+kadrs++;
+Debug.Log(kadrs);        
+    }
+    
+}
+
